23b8aa9147e527b101b0db9c5cfbc9a3f76479fd
Andrea Stagi <stagi.andrea@gmail.com>
2015-12-22 09:54:50 +0100

    Limit access to localhost

--- api/permissions.py
+++ api/permissions.py
@@ -4,4 +4,12 @@ from rest_framework import permissions
 class IsLocalhostRequest(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return True
+        return self._get_client_ip(request) == '127.0.0.1'
+
+    def _get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
